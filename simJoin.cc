37826a349b14d7bb3d5d7ced15c704e99f884fd1
Hari Amoor <amoor.hari@gmail.com>
2020-10-18 12:39:57 -0700

    Remove bad test harness

--- simJoin.cc
+++ simJoin.cc
@@ -30,41 +30,34 @@ bool simJoin::readData(const string &filename) {
  * error if the similarity join operation is failed.
  */
 
-int min(int x, int y, int z) 
-{ 
-    return min(min(x, y), z); 
-} 
-  
-int distance(string str1, string str2, int m, int n) 
-{ 
-    // If first string is empty, the only option is to 
-    // insert all characters of second string into first 
-    if (m == 0) 
-        return n; 
-  
-    // If second string is empty, the only option is to 
-    // remove all characters of first string 
-    if (n == 0) 
-        return m; 
-  
-    // If last characters of two strings are same, nothing 
-    // much to do. Ignore last characters and get count for 
-    // remaining strings. 
-    if (str1[m - 1] == str2[n - 1]) 
-        return distance(str1, str2, m - 1, n - 1); 
-  
-    // If last characters are not same, consider all three 
-    // operations on last character of first string, recursively 
-    // compute minimum cost for all three operations and take 
-    // minimum of three values. 
-    return 1 + min(distance(str1, str2, m, n - 1), // Insert 
-                   distance(str1, str2, m - 1, n), // Remove 
-                   distance(str1, str2, m - 1, n - 1) // Replace 
-                   ); 
-}
+int min(int x, int y, int z) { return min(min(x, y), z); }
 
-void test_distance() {
-  
+int distance(string str1, string str2, int m, int n) {
+  // If first string is empty, the only option is to
+  // insert all characters of second string into first
+  if (m == 0)
+    return n;
+
+  // If second string is empty, the only option is to
+  // remove all characters of first string
+  if (n == 0)
+    return m;
+
+  // If last characters of two strings are same, nothing
+  // much to do. Ignore last characters and get count for
+  // remaining strings.
+  if (str1[m - 1] == str2[n - 1])
+    return distance(str1, str2, m - 1, n - 1);
+
+
+  // If last characters are not same, consider all three
+  // operations on last character of first string, recursively
+  // compute minimum cost for all three operations and take
+  // minimum of three values.
+  return 1 + min(distance(str1, str2, m, n - 1),    // Insert
+                 distance(str1, str2, m - 1, n),    // Remove
+                 distance(str1, str2, m - 1, n - 1) // Replace
+             );
 }
 
 bool simJoin::SimilarityJoin(
